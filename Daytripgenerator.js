b8b1ff9687d6a01febce664432d317bb2151e261
Bertrand1012 <69489619+Bertrand1012@users.noreply.github.com>
2020-08-11 18:06:36 -0500

    starting to understand

--- Daytripgenerator.js
+++ Daytripgenerator.js
@@ -1,2 +1,47 @@
 "use strict";
 
+
+//function generateRandomDestination (Florida, Louisiana, Texas, Colorado, Arizona)
+
+ //   {   
+    
+   //         return Math.floor(Math.random() * (generateRandomDestination)+1);
+//console.log("generateRandomDestination");
+    //let generateRandomDestination = [];
+
+    //let Florida = generateRandomDestination[0];
+   // let Louisiana = generateRandomDestination[1];
+   // let Texas = generateRandomDestination[2];
+  //  let Colorado = generateRandomDestination[3];
+  //  let Arizona = generateRandomDestination[4];
+//generateRandomDestination = Math.floor(generateRandomDestination)+1;
+//console.log(runProgram);
+
+
+  //  }
+
+   //////// let generateRandomDestination = ['Florida', 'Louisiana', 'Texas', 'Colorado', 'Arizona'];{
+
+  /////  let Florida = generateRandomDestination[0];
+  /////  let Louisiana = generateRandomDestination[1];
+  ////  let Texas = generateRandomDestination[2];
+ /////  let Colorado = generateRandomDestination[3];
+  ///// let Arizona = generateRandomDestination[4];
+
+  /////let Destination = ['generateRandomDestination'.charAt(Math.floor(Math.random()*5))];
+//////console.log('Destination: ' + Destination);
+  ////  }
+
+  //  let generateRandomDestination = Math.random *4 (
+  //  generateRandomDestination = Math.floor(generateRandomDestination) +1
+  //  )
+
+  //function generateRandomDestination(Florida, Louisiana, Texas, Colorado, Arizona) {
+   // let result = '';
+   //         for (var i = length; i > 0;--i) result += 
+
+// }
+
+let destinations =  ['Florida', 'Louisiana', 'Texas', 'Colorado', 'Arizona'];
+let destination = destinations [Math.floor(Math.random()*destinations.length)];
+console.log('Destination is:' + destination);

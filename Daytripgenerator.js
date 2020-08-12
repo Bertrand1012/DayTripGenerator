13d4afd674311b700e3b9a787545a432e8fc1d14
Bertrand1012 <69489619+Bertrand1012@users.noreply.github.com>
2020-08-12 12:47:57 -0500

    adding new functions

--- Daytripgenerator.js
+++ Daytripgenerator.js
@@ -41,7 +41,107 @@
    //         for (var i = length; i > 0;--i) result += 
 
 // }
+function randomDestination()        { 
+    let destinations =  ['Florida', 'Louisiana', 'Texas', 'Colorado', 'Arizona'];
+    let destinationIndex = Math.floor(Math.random()*destinations.length);
+    let destination = destinations[destinationIndex];
+    return destination;
+                                    }
 
-let destinations =  ['Florida', 'Louisiana', 'Texas', 'Colorado', 'Arizona'];
-let destination = destinations [Math.floor(Math.random()*destinations.length)];
-console.log('Destination is:' + destination);
+
+function randomRestaurant()         {
+
+let Restaurants =  ['Burger King', 'Red Lobster', 'Taco Bell', 'Outback', 'Saltgrass'];
+let RestaurantIndex = Math.floor(Math.random()*Restaurants.length);
+let Restaurant = Restaurants[RestaurantIndex];
+return Restaurant;
+                                    }
+
+
+function randomTransportation()     {
+
+let Transportations =  ['Airplane', 'Car', 'Train', 'Bus', 'Walk'];
+let TransportationIndex = Math.floor(Math.random()*Transportations.length);
+let Transportation = Transportations[TransportationIndex];
+return Transportation;
+                                    }
+
+
+
+function randomEntertainment()      {
+
+let Entertainments =  ['Movies', 'Baseball Game', 'Read a Book', 'Ride Motorcycle', 'Tours'];
+let EntertainmentIndex = Math.floor(Math.random()*Entertainments.length);
+let Entertainment = Entertainments[EntertainmentIndex];
+return Entertainment;
+                                    }
+
+
+
+let chosenDestination = randomDestination();
+let chosenRestaurant = randomRestaurant();
+let chosenTransportation = randomTransportation();
+let chosenEntertainment= randomEntertainment();
+
+console.log('Destination is:' + chosenDestination);
+console.log('Restaurant is:' + chosenRestaurant);
+console.log('Transportation is:' + chosenTransportation);
+console.log('Entertainment is:' + chosenEntertainment);
+
+
+
+
+
+
+
+
+//User prompts answer yes or no if no run fiunction again if yes complete
+
+
+function like() {
+    let askAgain = prompt(" Do you like this? yes or no");
+    return askAgain;
+                }
+
+function tryAgain() {
+let reselect = prompt("Are you happy with the Itinerary? Enter yes or no get a new one.");
+return reselect;
+                    }
+
+let newItin = "Your new Itinerary is: traveling to " + chosenDestination + 
+" and dining at " + chosenRestaurant + ". You will be arriving by " 
++ chosenTransportation + " and your enterainment is " + chosenEntertainment + ".";
+
+let Intinerary = "Your destination is "  + chosenDestination + " and you are dining at "
+ + chosenRestaurant + ". The mode of transportation will be " + chosenTransportation +
+  " and your entertainment is " + chosenEntertainment + "."; 
+
+  alert (Intinerary);
+  console.log (Intinerary)
+
+let answer = "no";
+
+while (answer !== "yes") {
+    answer = like();
+ 
+    if (answer === "yes") {
+        alert("Great, your trip details are complete");
+        break;
+        
+    }
+    else if (answer === "no"){
+        //let chosenDestination = randomDestination();
+
+
+   answer = alert(newItin);
+
+    
+  console.log (newItin)
+    
+
+   
+
+}
+}
+
+
